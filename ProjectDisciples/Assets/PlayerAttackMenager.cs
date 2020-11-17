ddbbfa13dc9923a48cd5a1e75c9071ae526bbbf5
koppertje24 <32428243+koppertje24@users.noreply.github.com>
2020-11-17 14:33:43 +0100

    MergeSave
    This is a Merge Save

---
+++ ProjectDisciples/Assets/PlayerAttackMenager.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAttackMenager : MonoBehaviour
+{
+    [SerializeField] private  _firstAttack;
+
+    public void FerstAttack()
+    {
+        ((IPlayerAttack)_firstAttack).Attack();
+    }
+}
