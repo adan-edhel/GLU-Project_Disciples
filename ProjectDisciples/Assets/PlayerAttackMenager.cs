2f1f88ba0fd6f278c20a9f7a931e6e8268a69fd8
koppertje24 <32428243+koppertje24@users.noreply.github.com>
2020-11-23 15:14:19 +0100

    first steps of attacking
    this are the first steps of attacking

--- ProjectDisciples/Assets/PlayerAttackMenager.cs
+++ ProjectDisciples/Assets/PlayerAttackMenager.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Photon.Pun;
 
 public enum EPlayerElement
 {
@@ -16,12 +17,52 @@ public class PlayerAttackMenager : MonoBehaviour, ICharacterAttack
     [SerializeField] private EPlayerElement _CurrentElement = EPlayerElement.Fire;
     [SerializeField] private Animator _animetor;
 
+    [Header("renderer")]
+    [SerializeField] private SpriteRenderer _SpriteRenderer;
+ 
+    [Header("fire")]
+    [SerializeField] private string _FireFirstAtackPath = "Elements/Fire/BaseAtack";
+    [SerializeField] private Vector2 _firevelocity;
+    [SerializeField] private string _FireSecondAttackPath;
+
+    [Header("Water")]
+    [SerializeField] private string _WaterFirstAtackPath = "Elements/Water/BaseAtack";
+    [SerializeField] private string _WaterSecondAttackPath;
+
     private void Start()
     {
         if (_KnownElements == null)
         {
             _KnownElements = new List<EPlayerElement>();
         }
+    }
+
+    public void TestShoot() => Attack1();
+
+
+    private void FixedUpdate()
+    {
+
+        //update animator;
+        //_animetor.SetFloat("Element", (float)_CurrentElement);
+        //switch (_CurrentElement)
+        //{
+        //    case EPlayerElement.Fire:
+        //        _bandana.color = _FireColor;
+        //        break;
+        //    case EPlayerElement.Water:
+        //        _bandana.color = _WaterColor;
+        //        break;
+        //    case EPlayerElement.wind:
+        //        _bandana.color = _WindColor;
+        //        break;
+        //    case EPlayerElement.earth:
+        //        _bandana.color = _EarthColor;
+        //        break;
+        //    default:
+        //        _bandana.color = Color.magenta;
+        //        break;
+        //}
     }
 
 
@@ -118,7 +159,31 @@ public class PlayerAttackMenager : MonoBehaviour, ICharacterAttack
 
     private void FireAttack1()
     {
+        GameObject G;
+        if (PhotonNetwork.InRoom)
+        {
+            G = PhotonNetwork.Instantiate(_FireFirstAtackPath, transform.position, Quaternion.identity);
+        }
+        else
+        {
+            G = Instantiate(Resources.Load(_FireFirstAtackPath) as GameObject, transform.position, Quaternion.identity);
+        }
+        G.layer = gameObject.layer;
 
+        Rigidbody2D RB = G.GetComponent<Rigidbody2D>();
+        if (RB != null)
+        {
+            Vector2 NewVelocity = new Vector2(_firevelocity.x * (_SpriteRenderer.flipY ? 1 : -1), _firevelocity.y);
+            RB.velocity = NewVelocity;
+        }
+
+        ObjectleDamage OD = G.GetComponent<ObjectleDamage>();
+        if (OD != null)
+        {
+            OD.SetSender = this.gameObject;
+        }
+
+        Destroy(G, 5);
     }
 
     private void FireAttack2()
@@ -131,7 +196,30 @@ public class PlayerAttackMenager : MonoBehaviour, ICharacterAttack
     #region Water
     private void WaterAttack1()
     {
+        GameObject G;
+        if (PhotonNetwork.InRoom)
+        {
+            G = PhotonNetwork.Instantiate(_WaterFirstAtackPath, transform.position, Quaternion.identity);
+        }
+        else
+        {
+            G = Instantiate(Resources.Load(_WaterFirstAtackPath) as GameObject, transform.position, Quaternion.identity);
+        }
+        G.layer = gameObject.layer;
 
+        Rigidbody2D RB = G.GetComponent<Rigidbody2D>();
+        if (RB != null)
+        {
+            Vector2 NewVelocity = new Vector2(_firevelocity.x * (_SpriteRenderer.flipY ? 1 : -1), _firevelocity.y);
+            RB.velocity = NewVelocity;
+        }
+
+        ObjectleDamage OD = G.GetComponent<ObjectleDamage>();
+        if (OD != null)
+        {
+            OD.SetSender = this.gameObject;
+        }
+        Destroy(G,5);
     }
 
     private void WaterAttack2()
