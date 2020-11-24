2f55f7016e5fcb3bd27ceeb72825b45c4c06599f
Simone <529336@student.glu.nl>
2020-11-24 15:17:29 +0100

    in-game TimerUI

---
+++ ProjectDisciples/Assets/Code/UI/Timer.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class Timer : MonoBehaviour
+{
+    public TMP_Text timerText;
+    public float timer;
+    private static bool timeStarted = true;
+
+    // Update is called once per frame
+    private void Update()
+    {
+        if (timeStarted == true)
+        {
+            timer -= Time.deltaTime;
+
+            string minutes = Mathf.Floor(timer / 60).ToString("00");
+            string seconds = Mathf.Floor(timer % 60).ToString("00");
+
+            timerText.text = (string.Format("{0}:{1}", minutes, seconds));
+        }
+    }
+}
