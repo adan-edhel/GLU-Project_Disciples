4f1c957c884283123d8fbb1eda15638200696c7e
Simone <529336@student.glu.nl>
2020-11-24 11:06:55 +0100

    Health bar proto.

---
+++ ProjectDisciples/Assets/Code/UI/HealthUI.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthUI : MonoBehaviour
+{
+    public float health;
+
+    public float barDisplay;
+    public Vector2 position = new Vector2(20, 40);
+    public Vector2 size = new Vector2(60, 20);
+    public Texture2D emptyText;
+    public Texture2D fullText;
+
+    private void OnGUI()
+    {
+        GUI.BeginGroup(new Rect(position.x, position.y, size.x, size.y));
+        GUI.Box(new Rect(0, 0, size.x, size.y), emptyText);
+
+        GUI.BeginGroup(new Rect(0, 0, size.x * barDisplay, size.y));
+        GUI.Box(new Rect(0, 0, size.x, size.y), fullText);
+        GUI.EndGroup();
+        GUI.EndGroup();
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        barDisplay = health;
+    }
+}
