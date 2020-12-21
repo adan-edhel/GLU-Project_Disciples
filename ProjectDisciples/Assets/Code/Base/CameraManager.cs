5b0d68c07dd8c76418600dc5f6913d0c53da6add
MortifiedPotato <44699487+MortifiedPotato@users.noreply.github.com>
2020-12-21 14:05:36 +0100

    Changed Character Info & Optimized ShakeCamera Function

--- ProjectDisciples/Assets/Code/Base/CameraManager.cs
+++ ProjectDisciples/Assets/Code/Base/CameraManager.cs
@@ -102,7 +102,7 @@ public class CameraManager : MonoBehaviour
     /// <summary>
     /// Shakes the camera with the following parameters: Duration, Amplitude, Frequency. -- Duration always needs to be given a value, amplitude and frequency will use default value when left at 0. Defaults: Amp(1.2f), Freq(2.0f).
     /// </summary>
-    public void ShakeCamera(float duration, float amplitude, float frequency)
+    public void ShakeCamera(float duration, float amplitude = 0, float frequency = 0)
     {
         ShakeElapsedTime = duration / 10;
 
