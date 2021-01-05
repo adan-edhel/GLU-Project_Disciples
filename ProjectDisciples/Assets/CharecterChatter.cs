a6aebe0904e3efd15161616639c9c46d8571ffcf
koppertje24 <32428243+koppertje24@users.noreply.github.com>
2021-01-05 20:01:03 +0100

    basic chatter

---
+++ ProjectDisciples/Assets/CharecterChatter.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using Photon.Pun;
+using Photon.Realtime;
+
+public class CharecterChatter : MonoBehaviourPunCallbacks
+{
+    [SerializeField] private ChatterPanel _textPanel;
+
+    private void Awake()
+    {
+        if (photonView.IsMine)
+        {
+            _textPanel.SetAsStatic();
+        }
+        else
+        {
+            Destroy(transform.GetChild(0).gameObject);
+        }
+
+    }
+
+    [PunRPC]
+    public void SetMessage(string Message)
+    {
+        if (ChatterPanel.Instance!=null)
+        {
+            ChatterPanel.Instance.SetMessage(Message);
+        }
+    }
+
+    public void RPCSendMessage(string Message)
+    {
+        photonView.RPC("SendMessage", RpcTarget.All, Message);
+    }
+
+    public override void OnPlayerEnteredRoom(Player newPlayer)
+    {
+        if (photonView.IsMine)
+        {
+            SetMessage($"{newPlayer.NickName} has joined\n");
+        }
+    }
+
+    public override void OnPlayerLeftRoom(Player otherPlayer)
+    {
+        if (photonView.IsMine)
+        {
+            SetMessage($"{otherPlayer.NickName} has Left\n");
+        }
+    }
+
+}
---
+++ ProjectDisciples/Assets/ChatterPanel.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using TMPro;
+
+public class ChatterPanel : MonoBehaviour
+{
+    public static ChatterPanel Instance;
+    [SerializeField] private TMP_Text _textField;
+
+    private void Start()
+    {
+        _textField.text = string.Empty;
+    }
+
+    public void SetAsStatic() => Instance = this;
+
+    public void SetMessage(string Message)
+    {
+        _textField.text += Message;
+    }
+}
