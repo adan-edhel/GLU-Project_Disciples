c2f1b1f598ec0271e5ae9595ddfc324f164978f3
koppertje24 <32428243+koppertje24@users.noreply.github.com>
2021-01-25 14:01:15 +0100

    merge backup 25/01/2022

---
+++ ProjectDisciples/Assets/flashlightRotater.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+ 
+public class flashlightRotater : MonoBehaviourPunCallbacks
+{
+    [SerializeField] private GameObject _flashlicht;
+    [SerializeField] private CharacterAim _characterAimGameobject;
+    void Start()
+    {
+        if (!photonView.IsMine)
+        {
+            Destroy(_flashlicht);
+            Destroy(GetComponent<flashlightRotater>());
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        _flashlicht.transform.rotation = Quaternion.Euler(0, 0, Vector2.Angle(gameObject.transform.up, _characterAimGameobject.aimDirection));
+    }
+}
